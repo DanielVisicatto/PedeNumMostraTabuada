a91a04bb783e6d964136a14b4768bfe45370545f
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-03 14:12:15 -0300

    Add project files.

---
+++ PedeNumMostraTabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Digite um número de 1 a 9: ");
+if(!int.TryParse(Console.ReadLine(), out var numb))
+{
+    Console.WriteLine("Preciso de um número inteiro de 1 a 9...");
+}
+else
+{
+    if (numb < 1 || numb > 9)
+    {
+        Console.WriteLine("Entre 1 e 9 por favor");
+    }
+    else
+    {
+        Console.WriteLine("Você escolheu o {0}.\n\n" +
+        "Tome-lhe a tabuada do {0}:\n", numb);
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("{0} X {1} = {2}", numb, i, (numb * i));
+        }
+    }
+    
+}
